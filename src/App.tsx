4503b77c330fbff4cfcbfced47c0d7ee7f8b9841
Izabelle-21 <izabelleneubaner.taj@outlook.com>
2022-02-17 15:39:20 -0200

    First Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from "./components/List"
+import AddToList from './components/AddList';
+
+export interface MyState{
+  people:{
+    name:string;
+    age:number,
+    url:string;
+    note?:string;
+  }[]
+}
+
 
 function App() {
+
+  const[people, setPeople] = useState<MyState["people"]>([
+    {
+      name: "Stephen Curry",
+      url:"https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3975.png&w=350&h=254",
+      age:33,
+      note: "Best player of the entire NBA"
+    }
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>First Project</h1>
+      <List people={people}></List>
+      <AddToList people={people} setPeople={setPeople}/>
     </div>
+    
   );
 }
 
